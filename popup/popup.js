c1ebb5d158241e220077d7f86ddd32727be2c3b5
Berk Emre Uzun <54277320+Truirer@users.noreply.github.com>
2023-06-01 20:46:46 +0300

    bug fix

--- popup/popup.js
+++ popup/popup.js
@@ -24,11 +24,11 @@ resizeSlider.addEventListener('input', function() {
     });
 
 });
-let pxSlider = document.querySelector("#pxRatio");
-pxSlider.addEventListener('input', function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {data: {pxRatio:pxSlider.value}}, function(response) {
-        });
-    });
+// let pxSlider = document.querySelector("#pxRatio");
+// pxSlider.addEventListener('input', function() {
+//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//         chrome.tabs.sendMessage(tabs[0].id, {data: {pxRatio:pxSlider.value}}, function(response) {
+//         });
+//     });
 
-});
+// });
